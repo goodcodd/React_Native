373739bb960d5876d0660d00ab8b69c9dfb9e922
Anastasiia Lytvyn <109170684+goodcodd@users.noreply.github.com>
2024-04-21 12:37:13 +0300

    Update IconButton.js

--- mealsApp/components/IconButton.js
+++ mealsApp/components/IconButton.js
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet } from "react-native-web";
+import { Pressable, StyleSheet } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
 
 function IconButton({ onPress, icon, color }) {
